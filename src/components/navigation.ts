88db1cd9db7f4aae9fac696786d1b0825d0003a4
lucsoft <mail@lucsoft.de>
2021-02-07 13:59:14 +0100

    Changed Navigation url from HmSYS to ref the new status page

--- src/components/navigation.ts
+++ src/components/navigation.ts
@@ -5,7 +5,7 @@ import '../styles/navigation.css';
 const navigationElements = [
     { title: 'lucsoft.de', url: 'https://lucsoft.de' },
     { title: 'GitHub', url: 'https://github.com/lucsoft' },
-    { title: 'HmSYS', url: 'https://hmsys.de' },
+    { title: 'HmSYS', url: 'https://lucsoft.de/p/hmsys' },
     { title: 'Contact', url: 'mailto:mail@lucsoft.de' }
 ];
 

0192f7c91eb9e76a129016413af6a7667361c30a
lucsoft <mail@lucsoft.de>
2021-04-04 16:04:36 +0200

    Updated Twitter link

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -6,7 +6,7 @@ export const renderFooter = () => ({
     draw: () => {
         const footer = span(undefined, 'footer');
         const right = span(undefined);
-        right.innerHTML = `<a href="https://hmsys.de/">HmSYS</a><a href="https://github.com/lucsoft">GitHub</a><a href="https://twitter.com/lucsoft_">Twitter</a><a href="https://lucsoft.de/p/imprint">Imprint</a>`
+        right.innerHTML = `<a href="https://hmsys.de/">HmSYS</a><a href="https://github.com/lucsoft">GitHub</a><a href="https://twitter.com/lucsoft">Twitter</a><a href="https://lucsoft.de/p/imprint">Imprint</a>`
         footer.append(span("lucsoft.de – Copyright 2021"), right);
         return footer;
     }

3d144cddc4f322da02ddba209e03405c590d7622
lucsoft <mail@lucsoft.de>
2022-01-02 17:45:30 +0100

    refactor(core): Deleted Old Website code. Pages get reimplemnted and now get webgen version independent

--- src/pages/hangman/ai/aiType.ts
+++
@@ -1,9 +0,0 @@
-export type aiData = {
-    availableWords: string[];
-    winningChance: number;
-    highestProbability: {
-        char: string
-        count: number
-        procent: number
-    }[];
-};

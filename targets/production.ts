ef77a9c161ec8d8439918dbc911c94185b99563f
lucsoft <mail@lucsoft.de>
2022-01-03 13:21:06 +0100

    chore: version bump

--- targets/production.ts
+++ targets/production.ts
@@ -1,6 +1,6 @@
-import { build } from "https://deno.land/x/esbuild@v0.14.6/mod.js";
+import { build } from "https://deno.land/x/esbuild@v0.14.10/mod.js";
 import { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports/index.ts";
-import { emptyDirSync } from "https://deno.land/std@0.118.0/fs/mod.ts";
+import { emptyDirSync } from "https://deno.land/std@0.120.0/fs/mod.ts";
 emptyDirSync("dist");
 Deno.copyFileSync("index.html", "dist/index.html");
 

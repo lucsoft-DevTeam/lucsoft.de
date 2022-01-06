02cb5090e4f87ff3b1b7fd5cf1501915765f7858
lucsoft <mail@lucsoft.de>
2022-01-06 17:43:26 +0100

    refactor(core): Deleted NodeJS Releated Stuff added two new esbuild/deno targers

---
+++ targets/dev.ts
@@ -0,0 +1,35 @@
+import { serve } from "https://deno.land/x/esbuild@v0.14.6/mod.js";
+import { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports/index.ts";
+import { emptyDirSync } from "https://deno.land/std@0.118.0/fs/mod.ts";
+emptyDirSync("dist");
+Deno.copyFileSync("index.html", "dist/index.html");
+console.log("🚀 WebServer @ http://localhost:8080");
+serve({
+    port: 8080,
+    servedir: ".",
+    onRequest: ({ method, remoteAddress, path }) =>
+        console.log(
+            "📦",
+            method,
+            `http://localhost${path} from ${remoteAddress}`,
+        ),
+}, {
+    loader: {
+        ".woff": "file",
+        ".woff2": "file",
+        ".html": "file",
+        ".svg": "file",
+        ".png": "file",
+    },
+    plugins: [httpImports({
+        onCacheMiss: (path) => console.log("📡", "Loading", path),
+    })],
+    bundle: true,
+    entryPoints: {
+        "landing": "./pages/landing.ts",
+    },
+    minify: true,
+    splitting: true,
+    format: "esm",
+    logLevel: "info",
+});
---
+++ targets/production.ts
@@ -0,0 +1,28 @@
+import { build } from "https://deno.land/x/esbuild@v0.14.6/mod.js";
+import { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports/index.ts";
+import { emptyDirSync } from "https://deno.land/std@0.118.0/fs/mod.ts";
+emptyDirSync("dist");
+Deno.copyFileSync("index.html", "dist/index.html");
+
+await build({
+    loader: {
+        ".woff": "file",
+        ".woff2": "file",
+        ".html": "file",
+        ".svg": "file",
+        ".png": "file",
+    },
+    plugins: [httpImports({
+        onCacheMiss: (path) => console.log("📡", "Loading", path),
+    })],
+    bundle: true,
+    entryPoints: {
+        "landing": "./pages/landing.ts",
+    },
+    outdir: "dist",
+    minify: true,
+    splitting: true,
+    format: "esm",
+    logLevel: "info",
+});
+Deno.exit();

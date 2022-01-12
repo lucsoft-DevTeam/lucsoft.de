f8a67a6b72c08f87a86d45b6c7f0775ba49ba75f
lucsoft <mail@lucsoft.de>
2022-01-12 22:02:00 +0100

    refactor: targers cleanup moved to common file

--- targets/dev.ts
+++ targets/dev.ts
@@ -1,35 +1,10 @@
 import { serve } from "https://deno.land/x/esbuild@v0.14.10/mod.js";
-import { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports/index.ts";
-import { emptyDirSync } from "https://deno.land/std@0.120.0/fs/mod.ts";
-emptyDirSync("dist");
-Deno.copyFileSync("index.html", "dist/index.html");
+import { config } from "./common.ts";
 console.log("🚀 WebServer @ http://localhost:8080");
+
 serve({
     port: 8080,
-    servedir: ".",
+    servedir: "dist",
     onRequest: ({ method, remoteAddress, path }) =>
-        console.log(
-            "📦",
-            method,
-            `http://localhost${path} from ${remoteAddress}`,
-        ),
-}, {
-    loader: {
-        ".woff": "file",
-        ".woff2": "file",
-        ".html": "file",
-        ".svg": "file",
-        ".png": "file",
-    },
-    plugins: [httpImports({
-        onCacheMiss: (path) => console.log("📡", "Loading", path),
-    })],
-    bundle: true,
-    entryPoints: {
-        "landing": "./pages/landing.ts",
-    },
-    minify: true,
-    splitting: true,
-    format: "esm",
-    logLevel: "info",
-});
+        console.log("📦", method, `http://localhost${path} from ${remoteAddress}`),
+}, config);
--- targets/production.ts
+++ targets/production.ts
@@ -1,28 +1,5 @@
 import { build } from "https://deno.land/x/esbuild@v0.14.10/mod.js";
-import { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports/index.ts";
-import { emptyDirSync } from "https://deno.land/std@0.120.0/fs/mod.ts";
-emptyDirSync("dist");
-Deno.copyFileSync("index.html", "dist/index.html");
+import { config } from "./common.ts";
 
-await build({
-    loader: {
-        ".woff": "file",
-        ".woff2": "file",
-        ".html": "file",
-        ".svg": "file",
-        ".png": "file",
-    },
-    plugins: [httpImports({
-        onCacheMiss: (path) => console.log("📡", "Loading", path),
-    })],
-    bundle: true,
-    entryPoints: {
-        "landing": "./pages/landing.ts",
-    },
-    outdir: "dist",
-    minify: true,
-    splitting: true,
-    format: "esm",
-    logLevel: "info",
-});
+await build(config);
 Deno.exit();

d95a819c7a7ba6a60ff960c8081ff135fef4a2e7
lucsoft <mail@lucsoft.de>
2022-02-26 16:41:57 +0100

    devchain: refactor targets to single serve file

--- serve.ts
+++ serve.ts
@@ -1,3 +1,4 @@
+import { serve, build } from "https://deno.land/x/esbuild@v0.14.13/mod.js";
 import { emptyDirSync } from "https://deno.land/std@0.122.0/fs/empty_dir.ts";
 import { copySync } from "https://deno.land/std@0.122.0/fs/copy.ts";
 import { BuildOptions } from "https://deno.land/x/esbuild@v0.14.18/mod.js";
@@ -42,3 +43,16 @@ export const config: BuildOptions = {
     format: "esm",
     logLevel: "info",
 };
+
+if (Deno.args[ 0 ] == "dev" || Deno.args.length === 0) {
+    console.log("🚀 WebServer @ http://localhost:1337");
+    await serve({
+        port: 1337,
+        servedir: "dist",
+        onRequest: ({ method, remoteAddress, path }) =>
+            console.log("📦", method, `http://localhost${path} from ${remoteAddress}`),
+    }, { ...config, minify: false, splitting: false });
+} else {
+    (await build(config));
+    Deno.exit();
+}
--- targets/production.ts
+++
@@ -1,5 +0,0 @@
-import { build } from "https://deno.land/x/esbuild@v0.14.13/mod.js";
-import { config } from "./common.ts";
-
-await build(config);
-Deno.exit();

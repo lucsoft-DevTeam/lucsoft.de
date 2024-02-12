cab92c8dad940eb45beb3a6230e4e0da942bcbf8
lucsoft <lucsoft@users.noreply.github.com>
2024-02-12 00:53:37 +0000

    Update dependencies

--- pages/games/nonogramm.ts
+++ pages/games/nonogramm.ts
@@ -1,4 +1,4 @@
-import { chunk } from "https://deno.land/std@0.214.0/collections/chunk.ts";
+import { chunk } from "https://deno.land/std@0.215.0/collections/chunk.ts";
 import { asPointer, Body, createElement, Custom, Label, Vertical, WebGen } from "webgen/mod.ts";
 import '../../assets/nonogramm.css';
 import { createNormilizer, rotatedNormalIndexArray, rotateMatrixN90D } from "../../helper/matrixMath.ts";

446da2f51bfa4bff191cce40bac7587e75b48ec4
lucsoft <mail@lucsoft.de>
2024-07-04 19:00:55 +0200

    Fix description of switch

--- pages/docs/searchables/2.switch.ts
+++ pages/docs/searchables/2.switch.ts
@@ -1,5 +1,5 @@
 //# Switch
-//> Tags visually represent categories/topics for easy content scanning and relevance.
+//> Switch visually represent a direct action, like turning on or off a setting.
 import { delay } from "https://deno.land/std@0.224.0/async/delay.ts";
 import { Body, Switch, asRef } from "webgen/mod.ts";
 

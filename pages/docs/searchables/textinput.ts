ae5b0e006e4e23c0683e4441f9704fa7ed02bea9
lucsoft <mail@lucsoft.de>
2024-07-22 21:19:22 +0200

    Use ref for single input

--- pages/docs/searchables/textinput.ts
+++ pages/docs/searchables/textinput.ts
@@ -1,15 +1,13 @@
 //# TextInput
 //> TextInputs provide flexibility for users to enter variable-length input, such as short phrases or long paragraphs.
-import { Body, Label, TextInput, Vertical, asState, ref } from "webgen/mod.ts";
+import { Body, Label, TextInput, Vertical, asRef, ref } from "webgen/mod.ts";
 
-const data = asState({
-    example: ""
-});
+const value = asRef("Hello");
 
 Body(
     Vertical(
         TextInput("text", "Placeholder")
-            .sync(data, "example"),
-        Label(ref`Length: ${data.$example.map(it => it.length)}`)
-    ).setGap("0.5rem")
+            .ref(value),
+        Label(ref`Length: ${value.map(it => it.length)}`)
+    ).setGap("0.5rem"),
 );

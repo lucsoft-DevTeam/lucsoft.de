b51668b0a078623191310ecc51f6d46b169db377
lucsoft <11093382+lucsoft@users.noreply.github.com>
2025-08-13 01:24:26 +0000

    Update dependencies

--- pages/docs/liveEditor.ts
+++ pages/docs/liveEditor.ts
@@ -1,5 +1,5 @@
-// import esbuildwasm from 'https://deno.land/x/esbuild@v0.25.8/esbuild.wasm';
-// import * as esbuild from 'https://deno.land/x/esbuild@v0.25.8/wasm.js';
+// import esbuildwasm from 'https://deno.land/x/esbuild@v0.25.9/esbuild.wasm';
+// import * as esbuild from 'https://deno.land/x/esbuild@v0.25.9/wasm.js';
 // import { httpImports } from "https://deno.land/x/esbuild_serve@1.5.0/features/httpImports.ts";
 
 // await esbuild.initialize({
